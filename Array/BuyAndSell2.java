b823be651fc4b21769fd47efb88e4febf236a28e
Radhika Kumbhar <radhikakumbhar36@gmail.com>
2024-07-16 22:34:04 +0530

    Adding Day 3 Problems Solution

---
+++ Array/BuyAndSell2.java
@@ -0,0 +1,25 @@
+package Array;
+
+//we can use this problem using dynamic programming also
+//explore that
+
+public class BuyAndSell2 {
+
+    public static int maxProfit(int[] arr){
+        int maxProfit=0;
+        int n=arr.length;
+        for (int i =1;i<n;i++){
+            if(arr[i]>arr[i-1]){
+                maxProfit+=arr[i]-arr[i-1];
+            }
+        }return maxProfit;    
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(arr));
+    }
+    
+}
---
+++ Array/SortColors.java
@@ -0,0 +1,72 @@
+package  Array;
+
+public class SortColors {
+
+    // public static void sortColors(int[] arr){
+    //     //better approach
+    //     int count1=0;
+    //     int count2=0;
+    //     int count3=0;
+
+    //     for(int i=0;i<arr.length;i++){
+    //         if(arr[i]==0){
+    //             count1++;
+    //         }
+    //         else if(arr[i]==1){
+    //             count2++;
+    //         }
+    //         else{
+    //             count3++;
+    //         }
+    //     }
+
+    //     for(int i=0;i<count1;i++){
+    //         arr[i]=0;
+    //     }
+
+    //     for(int i=count1;i<count1+count2;i++){
+    //         arr[i]=1;
+    //     }
+
+    //     for(int i=count1+count2;i<arr.length;i++){
+    //         arr[i]=2;
+    //     }
+    // }
+
+    //Optimised Approach
+    public static void sortColors(int[] arr){
+        int low = 0;
+        int mid = 0;
+        int high = arr.length-1;
+
+        while(mid<=high){
+            if(arr[mid]==0){
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else{
+                int temp = arr[mid];
+                arr[mid] = arr[high];
+                arr[high] = temp;
+                high--;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+
+        int[] arr = {2,0,2,1,1,0};
+        sortColors(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+       
+    }
+}
